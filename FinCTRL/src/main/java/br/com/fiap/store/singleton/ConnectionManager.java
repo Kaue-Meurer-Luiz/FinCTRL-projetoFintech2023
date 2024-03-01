ef09c8a6d2972cbeef08c26b56f7663b99e70298
Kaue Meurer <rm550961@fiap.com.br>
2024-02-29 22:54:18 -0300

    Commit 1.0

--- FinCTRL/src/main/java/br/com/fiap/store/singleton/ConnectionManager.java
+++ FinCTRL/src/main/java/br/com/fiap/store/singleton/ConnectionManager.java
@@ -22,7 +22,7 @@ public class ConnectionManager {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			
-			connection = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", "login", "senha");
+			connection = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", "rm550961", "090704");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
